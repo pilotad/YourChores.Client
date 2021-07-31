f50ab23c4fa0d0d47a47445aa4cea85d8b06e247
pilotad <82273957+pilotad@users.noreply.github.com>
2021-07-31 17:39:31 +0300

    Initialization

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [weather, setWeather] = useState("loading...");
+  const getWeather = async () => {
+    if (weather == "loading...") {
+      var res = await fetch('http://192.168.0.104:5001/weatherforecast');
+      var data = await res.json();
+      setWeather(data[0].summary);
+
+    }
+  }
+  getWeather();
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{weather}</Text>
       <StatusBar style="auto" />
     </View>
   );
